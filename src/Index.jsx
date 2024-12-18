e462eb3f1696b11bf59c02d719330dc770bdee68
Hakim Baidouri <hakim.baidouri@hotmail.com>
2024-12-18 15:19:15 +0100

    Searh bar and data for home page added

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,32 +1,45 @@
-import { Suspense } from "react";
-import { Await, useLoaderData, Link } from "react-router-dom";
-
+import { useState } from "react";
+import { useLoaderData, Link } from "react-router-dom";
 
 export default function Index() {
-  const data = useLoaderData();
+  const detailData = useLoaderData();
+
+  const [search, setSearch] = useState(""); // État pour la barre de recherche
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value.toLowerCase()); // Met à jour la saisie
+  };
+
+  const filteredPokemon = (data) => {
+    // Si la barre de recherche est vide, retourner tous les Pokémon
+    if (!search.trim()) {
+      return data;
+    }
+    // Sinon, filtrer par nom
+    return data.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(search)
+    );
+  };
 
   return (
     <div>
       <h1>Home page</h1>
-      <Suspense fallback={<div>Chargement de l'application...</div>}>
-        <Await resolve={data}>
-          {(data) => {
-            const sortedResults = [...data.results].sort((a, b) =>
-              a.name.localeCompare(b.name)
-            );
-
-            return (
-              <ul>
-                {sortedResults.map((result) => (
-                  <li key={result.name}>
-                    <Link to={`/${result.name}`}>{result.name}</Link>
-                  </li>
-                ))}
-              </ul>
-            );
-          }}
-        </Await>
-      </Suspense>
+      <input
+        type="text"
+        placeholder="Rechercher un Pokémon..."
+        value={search}
+        onChange={handleSearch}
+      />
+      <div className="pokemon-container">
+          {filteredPokemon(detailData).map((pokemon) => (
+            <div key={pokemon.name} className="card">
+              <Link to={`/${pokemon.name}`} className="pokemon-link">
+                <img src={pokemon.sprite} alt={pokemon.name} />
+                <p>{pokemon.name}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
     </div>
   );
 }
